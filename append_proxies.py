455274386cd3d572ffb13b6ff4c71c9a7f49377c
lululoid <ereiwn@gmail.com>
2024-07-31 21:16:38 +0700

    Update: Using pylint as linters and using black, autopep8 and isort as fixers

--- append_proxies.py
+++ append_proxies.py
@@ -1,11 +1,13 @@
-import sys
-import yaml
-import shutil
 import os
+import shutil
+import sys
+
+import yaml
+
 
 def load_yaml(file_path):
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             return yaml.safe_load(file)
     except yaml.YAMLError as e:
         print(f"Error loading YAML file {file_path}: {e}")
@@ -14,31 +16,43 @@ def load_yaml(file_path):
         print(f"Unexpected error loading file {file_path}: {e}")
         sys.exit(1)
 
+
 def save_yaml(file_path, data):
     try:
-        with open(file_path, 'w') as file:
-            yaml.dump(data, file, allow_unicode=True, sort_keys=False, default_flow_style=False, indent=2)
+        with open(file_path, "w") as file:
+            yaml.dump(
+                data,
+                file,
+                allow_unicode=True,
+                sort_keys=False,
+                default_flow_style=False,
+                indent=2,
+            )
     except Exception as e:
         print(f"Error saving YAML file {file_path}: {e}")
         sys.exit(1)
 
+
 def merge_proxies(new_proxies, old_proxies):
     try:
         # Check if both new_proxies and old_proxies have the 'proxies' key
-        if 'proxies' not in new_proxies or 'proxies' not in old_proxies:
+        if "proxies" not in new_proxies or "proxies" not in old_proxies:
             raise ValueError("Both YAML files must contain a 'proxies' key")
 
         # Create a dictionary to store proxies with keys as (uuid, path) tuples
-        merged_proxies = {(proxy['uuid'], proxy['ws-opts']['path']): proxy for proxy in old_proxies['proxies']}
+        merged_proxies = {
+            (proxy["uuid"], proxy["ws-opts"]["path"]): proxy
+            for proxy in old_proxies["proxies"]
+        }
         new_proxies_added = False
-        
-        for proxy in new_proxies['proxies']:
-            key = (proxy['uuid'], proxy['ws-opts']['path'])
+
+        for proxy in new_proxies["proxies"]:
+            key = (proxy["uuid"], proxy["ws-opts"]["path"])
             if key not in merged_proxies:
                 merged_proxies[key] = proxy
                 new_proxies_added = True
-        
-        return {'proxies': list(merged_proxies.values())}, new_proxies_added
+
+        return {"proxies": list(merged_proxies.values())}, new_proxies_added
     except KeyError as e:
         print(f"Key error during merge: {e}")
         sys.exit(1)
@@ -49,11 +63,13 @@ def merge_proxies(new_proxies, old_proxies):
         print(f"Unexpected error during merge: {e}")
         sys.exit(1)
 
+
 def backup_file(file_path):
     if os.path.exists(file_path):
         backup_path = f"{file_path}.bcp"
         shutil.copy2(file_path, backup_path)
         print(f"Backup created at {backup_path}")
+
 
 def main(new_proxies_path, old_proxies_path):
     # Create a backup of the old proxies file
@@ -65,14 +81,17 @@ def main(new_proxies_path, old_proxies_path):
 
     # Merge proxies and get the result
     merged_proxies, new_proxies_added = merge_proxies(new_proxies, old_proxies)
-    
+
     if new_proxies_added:
         save_yaml(old_proxies_path, merged_proxies)
-        print(f"Proxies from {new_proxies_path} have been merged into {old_proxies_path} without duplicates.")
+        print(
+            f"Proxies from {new_proxies_path} have been merged into {old_proxies_path} without duplicates."
+        )
     else:
         print("No new proxies to merge. The existing file remains unchanged.")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python append_proxies.py <new_proxies.yaml> <old_proxies.yaml>")
         sys.exit(1)
--- v2raysubtoyaml.py
+++ v2raysubtoyaml.py
@@ -1,112 +1,129 @@
 import base64
 import json
+import os
+import sys
+
 import requests
 import yaml
-import sys
-import os
+
 
 def decode_v2ray_subscription(url):
     try:
         response = requests.get(url)
         if response.status_code == 200:
-            decoded_data = base64.b64decode(response.text).decode('utf-8')
+            decoded_data = base64.b64decode(response.text).decode("utf-8")
             return decoded_data
         else:
-            raise Exception(f"Failed to fetch V2Ray subscription from {url}. Status code: {response.status_code}")
+            raise Exception(
+                f"Failed to fetch V2Ray subscription from {url}. Status code: {response.status_code}"
+            )
     except requests.exceptions.SSLError as e:
-        print(f"SSL error occurred while fetching V2Ray subscription from {url}: {e}")
+        print(
+            f"SSL error occurred while fetching V2Ray subscription from {url}: {e}")
         print("Check your internet connection\n")
         sys.exit(1)
     except requests.exceptions.RequestException as e:
-        print(f"An error occurred while fetching V2Ray subscription from {url}: {e}")
+        print(
+            f"An error occurred while fetching V2Ray subscription from {url}: {e}")
         sys.exit(1)
 
+
 def convert_v2ray_to_clash(decoded_data):
-    v2ray_nodes = decoded_data.strip().split('\n')
-    clash_config = {
-        'proxies': []
-    }
-    
+    v2ray_nodes = decoded_data.strip().split("\n")
+    clash_config = {"proxies": []}
+
     for node in v2ray_nodes:
-        if node.startswith('vmess://'):
-            node_data = base64.b64decode(node[8:]).decode('utf-8')
+        if node.startswith("vmess://"):
+            node_data = base64.b64decode(node[8:]).decode("utf-8")
             try:
                 node_json = json.loads(node_data)
-                servername = node_json.get('servername', '')
+                servername = node_json.get("servername", "")
                 if not servername:
-                    servername = node_json.get('host', '')
-                
+                    servername = node_json.get("host", "")
+
                 clash_node = {
-                    'name': node_json.get('ps', 'Unnamed'),
-                    'server': node_json.get('add', 'unknown'),
-                    'port': int(node_json.get('port', 443)),
-                    'type': node[:5],
-                    'uuid': node_json.get('id', ''),
-                    'alterId': int(node_json.get('aid', 0)),
-                    'cipher': node_json.get('cipher', 'auto'),
-                    'tls': node_json.get('tls', '') == 'tls',
-                    'skip-cert-verify': node_json.get('skip-cert-verify', True),
-                    'servername': servername,
-                    'network': node_json.get('net', 'tcp'),
-                    'ws-opts': {
-                        'path': node_json.get('path', '/'),
-                        'headers': {'Host': node_json.get('host', '')}
-                    }
+                    "name": node_json.get("ps", "Unnamed"),
+                    "server": node_json.get("add", "unknown"),
+                    "port": int(node_json.get("port", 443)),
+                    "type": node[:5],
+                    "uuid": node_json.get("id", ""),
+                    "alterId": int(node_json.get("aid", 0)),
+                    "cipher": node_json.get("cipher", "auto"),
+                    "tls": node_json.get("tls", "") == "tls",
+                    "skip-cert-verify": node_json.get("skip-cert-verify", True),
+                    "servername": servername,
+                    "network": node_json.get("net", "tcp"),
+                    "ws-opts": {
+                        "path": node_json.get("path", "/"),
+                        "headers": {"Host": node_json.get("host", "")},
+                    },
                 }
-                clash_config['proxies'].append(clash_node)
+                clash_config["proxies"].append(clash_node)
             except json.JSONDecodeError:
                 print(f"Failed to decode JSON: {node_data}")
             except KeyError as e:
                 print(f"Missing key {e} in node: {node_data}")
-    
+
     return clash_config
 
+
 def filter_proxies_by_port(config, port):
-    filtered_proxies = [proxy for proxy in config['proxies'] if proxy.get('port') == port]
-    return {'proxies': filtered_proxies}
+    filtered_proxies = [
+        proxy for proxy in config["proxies"] if proxy.get("port") == port
+    ]
+    return {"proxies": filtered_proxies}
+
 
 def update_server(config, new_server):
-    for proxy in config['proxies']:
-        proxy['server'] = new_server
+    for proxy in config["proxies"]:
+        proxy["server"] = new_server
     return config
 
+
 def save_yaml(filename, data):
-    yaml_str = yaml.dump(data, allow_unicode=True, sort_keys=False, default_flow_style=False, indent=2)
-    
+    yaml_str = yaml.dump(
+        data, allow_unicode=True, sort_keys=False, default_flow_style=False, indent=2
+    )
+
     # Split the YAML string into lines
-    lines = yaml_str.split('\n')
-    
+    lines = yaml_str.split("\n")
+
     # Adjust indentation
     modified_lines = []
     for line in lines:
-        if line.startswith('proxies:'):
+        if line.startswith("proxies:"):
             # Do not add extra indentation to the 'proxies:' line
             modified_lines.append(line)
         elif line:
             # Add 2 spaces for indentation to all other lines
-            modified_lines.append('  ' + line)
-    
-    yaml_str_with_spaces = '\n'.join(modified_lines)
-    
-    with open(filename, 'w') as file:
+            modified_lines.append("  " + line)
+
+    yaml_str_with_spaces = "\n".join(modified_lines)
+
+    with open(filename, "w") as file:
         file.write(yaml_str_with_spaces)
     print(f"Configuration has been written to {filename}")
+
 
 def load_existing_proxies(filename):
     if os.path.exists(filename):
         try:
-            with open(filename, 'r') as file:
+            with open(filename, "r") as file:
                 return yaml.safe_load(file)
         except yaml.YAMLError as e:
             print(f"Error reading YAML file {filename}: {e}")
-            return {'proxies': []}
+            return {"proxies": []}
     else:
         return None
 
+
 def compare_proxies(new_config, existing_config):
-    new_proxies = {json.dumps(proxy, sort_keys=True) for proxy in new_config['proxies']}
-    existing_proxies = {json.dumps(proxy, sort_keys=True) for proxy in existing_config['proxies']}
-    
+    new_proxies = {json.dumps(proxy, sort_keys=True)
+                   for proxy in new_config["proxies"]}
+    existing_proxies = {
+        json.dumps(proxy, sort_keys=True) for proxy in existing_config["proxies"]
+    }
+
     added_proxies = new_proxies - existing_proxies
     if added_proxies:
         print("New proxies found:")
@@ -117,40 +134,42 @@ def compare_proxies(new_config, existing_config):
         print("No update available")
         return False
 
+
 def main():
-    v2ray_subscription_url = 'https://raw.githubusercontent.com/barry-far/V2ray-Configs/main/Splitted-By-Protocol/vmess.txt'
+    v2ray_subscription_url = "https://raw.githubusercontent.com/barry-far/V2ray-Configs/main/Splitted-By-Protocol/vmess.txt"
     decoded_data = decode_v2ray_subscription(v2ray_subscription_url)
     if decoded_data is None:
         return
-    
+
     clash_config = convert_v2ray_to_clash(decoded_data)
-    
+
     # Load existing proxies
-    existing_proxies = load_existing_proxies('proxies.yaml')
-    
+    existing_proxies = load_existing_proxies("proxies.yaml")
+
     # Check for updates
     if existing_proxies:
         if not compare_proxies(clash_config, existing_proxies):
             sys.exit(0)
-    
+
     # Save all proxies
-    save_yaml('proxies.yaml', clash_config)
-    
+    save_yaml("proxies.yaml", clash_config)
+
     # Filter and save proxies with port 80
     proxies_port_80 = filter_proxies_by_port(clash_config, 80)
-    save_yaml('proxies_port_80.yaml', proxies_port_80)
-    
+    save_yaml("proxies_port_80.yaml", proxies_port_80)
+
     # Filter and save proxies with port 443
     proxies_port_443 = filter_proxies_by_port(clash_config, 443)
-    save_yaml('proxies_port_443.yaml', proxies_port_443)
+    save_yaml("proxies_port_443.yaml", proxies_port_443)
 
     # Update server address and save
-    new_server = '104.26.6.171'  # Replace with the new server IP or hostname
+    new_server = "104.26.6.171"  # Replace with the new server IP or hostname
     updated_config_80 = update_server(proxies_port_80, new_server)
-    save_yaml('proxies_updated_80.yaml', updated_config_80)
-    
-    updated_config_443 = update_server(proxies_port_443, new_server)
-    save_yaml('proxies_updated_443.yaml', updated_config_443)
+    save_yaml("proxies_updated_80.yaml", updated_config_80)
 
-if __name__ == '__main__':
+    updated_config_443 = update_server(proxies_port_443, new_server)
+    save_yaml("proxies_updated_443.yaml", updated_config_443)
+
+
+if __name__ == "__main__":
     main()
